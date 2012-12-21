ab2ebbc683b97c29b0c3d5e6134bcf66506ef52e
Drew Chandler <drew@kongregate.com>
2012-12-21 14:28:08 -0800

    Don't rescue Tr8n::Config.current_locale_method

--- lib/tr8n/controller_methods.rb
+++ lib/tr8n/controller_methods.rb
@@ -66,15 +66,7 @@ module Tr8n::ControllerMethods
   def init_tr8n
     tr8n_current_locale = nil
     
-    begin
-      tr8n_current_locale = eval(Tr8n::Config.current_locale_method)
-    rescue Exception => ex
-      # fallback to the default session based locale implementation
-      # choose the first language from the accepted languages header
-      session[:locale] = tr8n_user_preffered_locale unless session[:locale]
-      session[:locale] = params[:locale] if params[:locale]
-      tr8n_current_locale = session[:locale]
-    end
+    tr8n_current_locale = eval(Tr8n::Config.current_locale_method)
     
     tr8n_current_user = nil
     if Tr8n::Config.site_user_info_enabled? 
