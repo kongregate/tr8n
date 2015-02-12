db7da178bf27b11ce34775e0530090b382e2362a
stopdropandrew <stopdropandrew@gmail.com>
2015-02-12 09:42:46 -0800

    Even more deprecation warnings

--- app/controllers/tr8n/api/v1/language_controller.rb
+++ app/controllers/tr8n/api/v1/language_controller.rb
@@ -23,7 +23,7 @@
 
 class Tr8n::Api::V1::LanguageController < Tr8n::Api::V1::BaseController
 
-  # for ssl access to the translator - using ssl_requirement plugin  
+  # for ssl access to the translator - using ssl_requirement plugin
   ssl_allowed :translate  if respond_to?(:ssl_allowed)
 
   # returns a list of all languages
@@ -34,17 +34,17 @@ class Tr8n::Api::V1::LanguageController < Tr8n::Api::V1::BaseController
     end
     sanitize_api_response({:languages => languages})
   end
-  
+
   def translate
     language = Tr8n::Language.for(params[:language]) || tr8n_current_language
-    source = CGI.unescape(params[:source] || "API") 
-    
+    source = CGI.unescape(params[:source] || "API")
+
     return sanitize_api_response(translate_phrase(language, params, {:source => source, :api => :translate})) if params[:label]
-    
+
     # API signature
     # {:source => "", :language => "", :phrases => [{:label => ""}]}
-    
-    # get all phrases for the specified source 
+
+    # get all phrases for the specified source
     # this can be used by a parallel application or a JavaScript Client SDK that needs to build a page cache
     if params[:batch] == "true" or params[:cache] == "true"
 
@@ -54,7 +54,7 @@ class Tr8n::Api::V1::LanguageController < Tr8n::Api::V1::BaseController
 
       source_names = params[:sources] || [params[:source]]
       translations = Tr8n::Cache.fetch("tr8n:api:v1:languages:translate:#{language.id}:#{source_names.join('-')}") do
-        sources = Tr8n::TranslationSource.find(:all, :conditions => ["source in (?)", source_names])
+        sources = Tr8n::TranslationSource.where(source: source_names)
         source_ids = sources.collect{|source| source.id}
 
         if source_ids.empty?
@@ -64,7 +64,7 @@ class Tr8n::Api::V1::LanguageController < Tr8n::Api::V1::BaseController
           conditions << source_ids.uniq
         end
 
-        Tr8n::TranslationKey.find(:all, :conditions => conditions).map do |tkey|
+        Tr8n::TranslationKey.where(conditions).map do |tkey|
           tkey.translate(language, {}, {:api => true, :api => :cache})
         end
       end
@@ -75,34 +75,34 @@ class Tr8n::Api::V1::LanguageController < Tr8n::Api::V1::BaseController
 
       return sanitize_api_response({:phrases => translations, :api => :translate})
     elsif params[:phrases]
-      
+
       phrases = []
       begin
         phrases = HashWithIndifferentAccess.new({:data => JSON.parse(params[:phrases])})[:data]
       rescue Exception => ex
         return sanitize_api_response({"error" => "Invalid request. JSON parsing failed: #{ex.message}"})
       end
-      
+
       translations = []
       phrases.each do |phrase|
         phrase = {:label => phrase} if phrase.is_a?(String)
         translations << translate_phrase(language, phrase, {:source => source, :api => :translate})
       end
-      
-      return sanitize_api_response({:phrases => translations})    
+
+      return sanitize_api_response({:phrases => translations})
     end
-    
+
     sanitize_api_response(:phrases => {})
   rescue Tr8n::KeyRegistrationException => ex
     sanitize_api_response({"error" => ex.message})
   end
 
 private
-  
+
   def translate_phrase(language, phrase, opts = {})
     return "" if phrase[:label].strip.blank?
     translation_key = Tr8n::TranslationKey.find_or_create(phrase[:label], phrase[:description], opts)
     translation_key.translate(language, {}, opts)
   end
-  
+
 end
